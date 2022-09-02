878e6a9e49c9d98a697fe8aa61290e407bbaa740
jay <jayleungg@gmail.com>
2022-09-01 20:37:38 -0400

    photos up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />

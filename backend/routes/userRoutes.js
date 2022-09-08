e3c5eacd41d8745a24b8f7d3b12d02ce0fe0922c
jay <jayleungg@gmail.com>
2022-09-08 13:42:34 -0400

    breaktime

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 import { getUser } from "../controllers/userController.js";
 import { getUserProfile } from "../controllers/userController.js";
 import { registerUser } from "../controllers/userController.js";
+import { UpdateProfile } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
-//localhost:4000/api/users/login
-
+//POST localhost:4000/api/users
 router.route("/").post(registerUser);
+
+//GET localhost:4000//api/users/login
 router.post("/login", getUser);
+
+//GET localhost:4000//api/users/profile
 router.route("/profile").get(protect, getUserProfile);
+
+//PUT localhost:4000//api/users/profile
+router.route("/profile").put(protect, UpdateProfile);
 
 export default router;

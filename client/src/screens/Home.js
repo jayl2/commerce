287a51b5ef625ad669a264d08a1361afe551ad47
jay <jayleungg@gmail.com>
2022-09-12 11:27:43 -0400

    working

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Product from "../components/Product";
 import Message from "../components/Message";
